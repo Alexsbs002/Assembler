cb756384255a0595c394db79cd2d9d06b4985a6a
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2021-03-17 20:44:14 +0600

    Update assembler lab 1 var 5.cpp

--- assembler lab 1 var 5.cpp
+++ assembler lab 1 var 5.cpp
@@ -1,4 +1,4 @@
-﻿// assembler lab 1 var 5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// assembler lab 1 var 5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -7,60 +7,60 @@ using namespace std;
 
 int main()
 {
-    //(b1 * (w1 - b2) - w2 * b3) / w3 + d1 / b4
-    char b1 = 100, b2 = 5, b3 = 3, b4 = 4; //1
-    short int w1 = 20000, w2 = 10000, w3 = 6;//2
-    int d1 = 10;//4
-    int res = 0;
-    _asm {
-        ; 1 = (w1 - b2) = bx
-        mov al, b2
-        cbw
-        mov bx, w1
-        sub bx, ax
 
-        ; 2 = b1 * 1 = bx
-        mov al, b1
-        cbw
-        mul bx
-        mov bx, ax
+	char b1 = 100, b2 = 5, b3 = 3, b4 = 4; //1
+	short int w1 = 20000, w2 = 10000, w3 = 6;//2
+	int d1 = 10;//4
+	int res = 0;
+	int p = (b1 * (w1 - b2) - w2 * b3) / w3 + d1 / b4;
+	_asm {
+		; 1 = (w1 - b2) = bx
+		mov al, b2
+		cbw
+		mov bx, w1
+		sub bx, ax
 
-        ; 3 = w2 * b3 = ax
-        mov al, b3
-        cbw
-        mul w2
+		; 2 = b1 * 1 = (cx:bx)
+		mov al, b1
+		cbw
+		imul bx
+		mov bx, ax
+		mov cx, dx
 
-        ; 4 = 2 - 3 = bx
-        sub bx, ax
+		; 3 = w2 * b3 = (dx:ax)
+		mov al, b3
+		cbw
+		mul w2
 
-        ; 5 = 4 / w3 = ax
-        mov ax, world ptr bx
-        mov dx, world ptr bx+2
-        div w3
+		; 4 = 2 - 3 = (cx:bx)
+		sub bx, ax
+		sbb cx, dx
 
-        ; 6 = d1 / b4 = eax:edx
-        mov eal, eax
-        mov edl, edx
-        mov al, b4
-        cbw
-        cwd
-        cdq; EDX:EAX = b4
+		; 5 = 4 / w3 = (cx, bx)
+		mov dx, cx
+		mov ax, bx
+		idiv w3
+		mov bx, ax
+		mov cx, dx
 
+		; 6 = d1 / b4 = (ax, dx)
 
-        idiv d1
-       
+		mov ax, word ptr d1
+		mov dx, word ptr d1 + 2
+		idiv b4
 
-        ; 7 = result
+		; 7 = 5 + 6
 
-        mow double word ptr res, eax
-        mow double word ptr res + 4, edx
-
-    }
-    
-    
-    
-    cout << res;
-    return 0;
+		add cx, ax
+		adc bx, dx
 
 
+
+		mov word ptr res, cx
+		mov word ptr res + 2, bx
+	}
+	cout << p << "\n";
+	cout << res << "\n";
+	return 0;
 }
+	
